b50c1330741cb333f260ff5d1cc123201319b6c5
seyityahya <seyityahya05@gmail.com>
2023-05-13 12:03:26 +0300

    navbar started

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,64 @@
-import React from "react";
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
 import classes from "./navbar.module.css";
+import person from "../../../public/person.jpg";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleShowDropdown = () => setShowDropdown((prev) => true);
+
+  const handleHideDropdown = () => setShowDropdown((prev) => false);
+
+  const loggedIn = true;
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.wrapper}>
+        <h2 className={classes.left}>
+          <Link href="/">BlogApp</Link>
+        </h2>
+        <ul className={classes.right}>
+          {loggedIn ? (
+            <div>
+              <Image
+                onClick={handleShowDropdown}
+                src={person}
+                width="45"
+                height="45"
+              />
+              {showDropdown && (
+                <div className={classes.dropdown}>
+                  <AiOutlineClose
+                    className={classes.closeIcon}
+                    onClick={handleHideDropdown}
+                  />
+                  <button
+                    onClick={handleHideDropdown}
+                    className={classes.logout}
+                  >
+                    Logout
+                  </button>
+                  <Link
+                    onClick={handleHideDropdown}
+                    href="/create-post"
+                    className={classes.create}
+                  >
+                    Create
+                  </Link>
+                </div>
+              )}
+            </div>
+          ) : (
+            <h2>asdfg</h2>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
